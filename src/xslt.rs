6b2005573e46d8bcca8a60b63368f37f3e7b2039
Jaden Carver <jaden.carver@gmail.com>
2017-04-14 16:48:48 -0400

    Only show diff when index is present. --- "BLBA-1234":   Description: ""   Developer: Sprint   Estimate: "5"   Status: Done

--- src/xslt.rs
+++ src/xslt.rs
@@ -64,7 +64,7 @@ pub fn panel_xslt() -> String {
                         }
                     }
                     xsl:choose {
-                        xsl:when test="/state/diffs" {
+                        xsl:when test="/state/diffs/*" {
                             xsl:apply-templates select="/state/diffs" {}
                         }
                         xsl:otherwise {
