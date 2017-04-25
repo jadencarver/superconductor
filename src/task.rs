a0ddfaf076137a236b8d9f8f403843835b5ee3d4
Jaden Carver <jaden.carver@gmail.com>
2017-04-25 00:47:56 -0400

    --- master:   Estimate: 12

--- src/task.rs
+++ src/task.rs
@@ -3,10 +3,11 @@ use yaml_rust::yaml::Hash;
 
 extern crate git2;
 use self::git2::{Repository, ObjectType};
-use self::git2::{Commit, Reference};
+use self::git2::{Commit, Reference, Oid};
 
 pub struct Task {
     pub name: String,
+    commit: Option<Oid>,
     changes: Hash
 }
 
@@ -19,7 +20,8 @@ impl Task {
         tasks.retain(|c| c.name == name);
         tasks.pop().unwrap_or(Task {
             name: String::from(name),
-            changes: Hash::new()
+            changes: Hash::new(),
+            commit: None
         })
     }
 
@@ -37,6 +39,7 @@ impl Task {
                                     tasks.push(Task {
                                         name: String::from(name),
                                         changes: values.clone(),
+                                        commit: Some(commit.id())
                                     });
                                 }
                             }
@@ -48,49 +51,73 @@ impl Task {
         tasks
     }
 
+    pub fn get(&self, repo: &Repository, property: &Yaml) -> Option<Yaml> {
+        if let Some(value) = self.changes.get(&property) {
+            Some(value.clone())
+        } else if let Some(commit) = self.commit {
+            let commit = repo.find_commit(commit).expect("Unable to find commit!");
+            let mut parents = vec![];
+            let mut candidates = vec![];
+            for parent in commit.parents() {
+                let mut tasks = Task::from_commit(repo, &self.name, &parent);
+                parents.append(&mut tasks);
+            }
+            for parent in parents.iter() {
+                if let Some(value) = parent.get(&repo, property) {
+                    candidates.push(value);
+                }
+            }
+            candidates.pop()
+        } else {
+            None
+        }
+    }
+
     pub fn changes(&self, repo: &Repository, commit: &Commit) -> Vec<(String, Option<String>, String)> {
         let mut changes = vec![];
-        let mut parents = vec![];
 
-        for parent in commit.parents() {
-            let mut walk = repo.revwalk().unwrap();
-            walk.set_sorting(git2::SORT_REVERSE);
-            if let Ok(_) = walk.push(parent.id()) {
-                for rev in walk {
-                    let parent_commit = repo.find_commit(rev.unwrap()).unwrap();
-                    parents.append(&mut Task::from_commit(repo, &self.name, &parent_commit));
-                }
-            }
-        }
-
-        for (property, value) in &self.changes {
-            let name = String::from(property.as_str().unwrap_or("[unknown]"));
-            let after = match value {
-                &Yaml::String(ref s) => s.clone(),
-                &Yaml::Integer(i) => format!("{}", i),
-                &Yaml::Boolean(b) => format!("{}", b),
-                _ => String::from("[unknown]")
-            };
-            let mut before = None;
-            for parent in parents.iter() {
-                if let Some(value) = parent.changes.get(property) {
-                    before = match value {
-                        &Yaml::String(ref s) => Some(s.clone()),
-                        &Yaml::Integer(i) => Some(format!("{}", i)),
-                        &Yaml::Boolean(b) => Some(format!("{}", b)),
-                        _ => None
-                    }
-                }
-            }
-            if let Some(before) = before {
-                if before != after {
-                    changes.push((name, Some(before), after));
-                }
-            } else {
-                changes.push((name, before, after));
+        let properties = ["Status","Estimate","Developer","Description"];
+        for property in properties.iter() {
+            let prop = Yaml::String(String::from(*property));
+            if let Some(value) = self.get(&repo, &prop) {
+                let after = match value {
+                    Yaml::String(ref s) => s.clone(),
+                    Yaml::Integer(i) => format!("{}", i),
+                    Yaml::Boolean(b) => format!("{}", b),
+                    _ => String::from("[unknown]")
+                };
+                changes.push((String::from(*property), None, after));
             }
         }
         changes
     }
+
+        //for (property, value) in &self.changes {
+        //    let name = String::from(property.as_str().unwrap_or("[unknown]"));
+        //    let after = match value {
+        //        &Yaml::String(ref s) => s.clone(),
+        //        &Yaml::Integer(i) => format!("{}", i),
+        //        &Yaml::Boolean(b) => format!("{}", b),
+        //        _ => String::from("[unknown]")
+        //    };
+        //    let mut before = None;
+        //    for parent in parents.iter() {
+        //        if let Some(value) = parent.get(&repo, property) {
+        //            before = match value {
+        //                Yaml::String(ref s) => Some(s.clone()),
+        //                Yaml::Integer(i) => Some(format!("{}", i)),
+        //                Yaml::Boolean(b) => Some(format!("{}", b)),
+        //                _ => None
+        //            }
+        //        }
+        //    }
+        //    if let Some(before) = before {
+        //        if before != after {
+        //            changes.push((name, Some(before), after));
+        //        }
+        //    } else {
+        //        changes.push((name, before, after));
+        //    }
+        //}
 
 }
