ed956d3ac5d25ee3cdffa37b2470b19a772f9a22
Jaden Carver <jaden.carver@gmail.com>
2017-05-30 02:08:26 -0400

    Updates screenshot

--- spec/integration/task_spec.rb
+++ spec/integration/task_spec.rb
@@ -4,7 +4,7 @@ RSpec.feature 'Managing tasks' do
   include IntegrationSpec::Screenshots
 
   it 'displays tasks by status', js: true do
-    visit '/'
+    visit '/docs/file/README.md'
     expect(page).to take_screenshot_of('tasks-by_status')
   end
 end
