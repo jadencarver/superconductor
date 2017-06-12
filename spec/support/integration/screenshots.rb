e3c277dcd82e7a6ec29fd8eaecbf3e355a152468
Jaden Carver <jcarver14@bloomberg.net>
2017-06-12 12:09:51 -0400

    Updated screenshot code

--- spec/support/integration/screenshots.rb
+++ spec/support/integration/screenshots.rb
@@ -1,45 +1,76 @@
-require 'ansi'
+module IntegrationSpec
+  module Screenshots
+    extend RSpec::Matchers::DSL
 
-module IntegrationSpec::Screenshots
-  extend RSpec::Matchers::DSL
-  
-  def tracked_screenshots
-    @@tracked_screenshots ||= begin
-                            Hash[`git ls-files -s spec/features/screenshots`.split("\n").map do |line|
-                              key, *values = line.split(/\s/).reverse; [key, values]
-                            end]
-                          end
-  end
+    SIZE = 18
+    WRAP = true
+    PATH = "spec/integration/screenshots"
 
-  matcher :take_screenshot_of do |name|
-    passed = true
-    printf ANSI.clear_line + "\n"
-    match do |page|
-      [
-        ["spec/integration/screenshots/#{name}.png", "Desktop", 2880/2, 1800/2 ],
-        #["spec/integration/screenshots/#{name}_ipad.png", "iPad Portrait", 768, 1024],
-        #["spec/integration/screenshots/#{name}_iphone6plus.png", "iPhone 6+", 414, 736 ],
-        #["spec/integration/screenshots/#{name}_iphone5.png", "iPhone 5", 320, 568 ],
-      ].inject(0) do |accum, (path, name, width, height)|
-        term_width = (width/height.to_f * (18 * 2.33)).ceil
-        # break if config.accum + term_width > ANSI::Terminal.terminal_width # overflow: hidden;
-        accum = 0 and puts if accum + term_width > ANSI::Terminal.terminal_width # overflow: wrap;
-        #page.driver.resize(width, height)
+    def tracked_screenshots
+      @@tracked_screenshots ||= begin
+                                  Hash[`git ls-files -s spec/features/screenshots`.split("\n").map do |line|
+                                    key, *values = line.split(/\s/).reverse; [key, values]
+                                  end]
+                                end
+    end
 
-        page.save_screenshot path
-        print ANSI.up(19) + ANSI.right(accum) if accum > 0
-        system("bin/imgcat #{path}")
-        print ANSI.right(accum) if accum > 0
-        puts ANSI.bold { name.center(term_width) }
-
-        if git_status = tracked_screenshots[path]
-          passed = false unless git_status[1] == `git hash-object #{path}`.chomp
+    def take_screenshot(options = {})
+      options = { options => true } if options.is_a?(Symbol)
+      page.save_screenshot(nil, options).tap do |path|
+        term_width, max_width = ANSI::Terminal.terminal_width, 125
+        if term_width > max_width then width = max_width
+        else width = term_width
         end
 
-        accum + term_width + 1
+        data = Base64.encode64(Rails.root.join(path).read)
+        puts "\n#{ANSI.right(term_width/2-width/2)}\033]1337;File=;inline=1;width=#{width}:#{data}\a\n\n"
       end
-      puts
-      return true if passed
     end
+    alias_method :tk, :take_screenshot
+
+    matcher :take_screenshot_of do |name|
+      passed = true
+      printf ANSI.clear_line + "\n"
+      match do |page|
+        [
+          ["#{name.parameterize}_iphone5.png", "iPhone 5", 320, 568 ],
+          ["#{name.parameterize}_iphone6plus.png", "iPhone 6+", 414, 736 ],
+          ["#{name.parameterize}_ipad.png", "iPad Portrait", 768, 1024],
+          ["#{name.parameterize}.png", "Desktop", 2880/2, 1800/2 ],
+        ].inject(0) do |accum, (filename, caption, width, height)|
+
+          path = "#{PATH}/#{filename}"
+          term_width = (width/height.to_f * (SIZE * 2.1)).ceil
+          break if !WRAP && accum + term_width > ANSI::Terminal.terminal_width
+          page.driver.resize(width, height)
+
+          page.save_screenshot Rails.root.join(path)
+          git_status = tracked_screenshots[path]
+
+          if git_status && git_status[1] != `git hash-object #{path}`.chomp
+            # BAD
+            passed = false
+            system("mkdir -p tmp/capybara")
+            system("git cat-file blob #{git_status[1]} | compare - #{path} tmp/capybara/diff_#{filename}")
+            system("git cat-file blob #{git_status[1]} | convert -delay 150 -resize x300 -loop 0 - -background Orange label:'After' -gravity Center -append #{path} tmp/capybara/diff_#{filename} tmp/capybara/diff_#{filename}.gif")
+
+            data = Base64.encode64(Rails.root.join("tmp/capybara/diff_#{filename}.gif").read)
+          else
+            # GOOD
+            data = Base64.encode64(Rails.root.join(path).read)
+          end
+
+          print ANSI.up(SIZE+1) + ANSI.right(accum) if accum > 0
+          puts "\033]1337;File=;inline=1;height=#{SIZE}:#{data}\a\n"
+          print ANSI.right(accum) if accum > 0
+          puts ANSI.bold { caption.center(term_width) }
+
+          accum + term_width
+        end
+        puts
+        return true if passed
+      end
+    end
+
   end
 end
